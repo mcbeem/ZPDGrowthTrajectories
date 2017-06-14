d0cc0f28b8e8e4d8cbb1fb5ab46a1340fccdf57a
mcbeem <mmcbee@gmail.com>
2017-06-14 14:28:49 -0400

    vignettes test

---
+++ R/onLoad.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname = find.package("ZPDGrowthTrajectories"), pkgname = "ZPDGrowthTrajectories") {
+  packageStartupMessage("Type vignette(\"Intro\", package=\"ZPDGrowthTrajectories\") to view the package vignette.")
+
+}
